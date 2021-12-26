6f8ddc4e30621b80b8e46b00622f82f3ee8c32cf
screamz2k <78593516+screamz2k@users.noreply.github.com>
2021-12-26 09:57:29 +0100

    Small bug fix

--- settings.py
+++ settings.py
@@ -5,4 +5,7 @@ load_dotenv()
 secret_key = getenv("secret_key")
 fernet = Fernet(getenv("dec_key").encode())
 user_agent = getenv("user_agent")
-agent_needed = bool(getenv("agent_needed"))
+if getenv("agent_needed") == "True":
+   agent_needed = True
+else:
+   agent_needed = False
